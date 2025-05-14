d9056dd04854c2914a45d4a3092d108bad222c90
Leandro Chinelli <chinelli15@gmail.com>
2025-05-14 15:33:46 -0300

    Adding route correction

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
